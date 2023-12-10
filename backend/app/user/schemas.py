845ddcf2d631b70bdd09695e67dc140a34a93df1
HiagoAC <hiago.uft@gmail.com>
2023-12-10 17:41:34 +0900

    added schemas for token api.

--- backend/app/user/schemas.py
+++ backend/app/user/schemas.py
@@ -2,11 +2,11 @@
 Schemas for the user API.
 """
 from django.contrib.auth import get_user_model
-from ninja import ModelSchema
+from ninja import ModelSchema, Schema
 
 
 class UserSchemaIn(ModelSchema):
-    """INPUT Schema for user model."""
+    """Input Schema for user model."""
     class Meta:
         model = get_user_model()
         fields = ['email', 'password']
@@ -14,8 +14,25 @@ class UserSchemaIn(ModelSchema):
 
 
 class UserSchemaOut(ModelSchema):
-    """OUTPUT Schema for user model."""
+    """Output Schema for user model."""
     class Meta:
         model = get_user_model()
         fields = ['email']
         optional_fields = ['first_name', 'last_name', ]
+
+
+class CredentialsSchema(Schema):
+    """Input Schema for user credentials."""
+    email: str
+    password: str
+
+
+class TokenSchema(Schema):
+    """Output Schema for the token api."""
+    access_token: str
+    refresh_token: str
+
+
+class ErrorSchema(Schema):
+    """Output Schema for errors."""
+    message: str
