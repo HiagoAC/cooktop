1764a307661701f14e026454b4cbfcc266053f55
HiagoAC <hiago.uft@gmail.com>
2023-12-21 21:17:54 +0900

    set configurations for development media files.

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -129,6 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'static/media'
+
+MEDIA_ROOT = 'vol/web/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
