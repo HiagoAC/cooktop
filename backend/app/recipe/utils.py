dd4caae999765d26b64815cb019d280962403bbb
HiagoAC <hiago.uft@gmail.com>
2024-01-07 16:03:55 +0900

    created annotate_by_ing_cout() function.

--- backend/app/recipe/utils.py
+++ backend/app/recipe/utils.py
@@ -6,6 +6,17 @@ from typing import List
 from recipe.models import Recipe
 
 
+def annotate_by_ing_count(
+        queryset: QuerySet[Recipe], ingredients: List[str]) -> QuerySet:
+    """annotate the number of matches in recipes of the given queryset."""
+    query_expression = Q(
+        recipeingredient__ingredient__name__in=ingredients)
+    return queryset.annotate(
+        match_count=Count(
+            'recipeingredient', filter=query_expression)
+            )
+
+
 def get_recipes_by_ings(
         queryset: QuerySet[Recipe], ingredients: List[str]) -> QuerySet:
     """
@@ -13,14 +24,7 @@ def get_recipes_by_ings(
     given list of ingredients the recipe contains.
     Recipes that contain 0 of the given ingredients are filtered out.
     """
-    # annotate the number of matches in recipes.
-    query_expression = Q(
-        recipeingredient__ingredient__name__in=ingredients)
-    queryset = queryset.annotate(
-        match_count=Count(
-            'recipeingredient', filter=query_expression)
-            )
-
+    queryset = annotate_by_ing_count(queryset, ingredients)
     # filter out 0 matches recipes and order by match count
     queryset = queryset.filter(match_count__gt=0).order_by('-match_count')
 
