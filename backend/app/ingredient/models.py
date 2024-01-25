3335b0c0a57c594ecb5205735ad62da24898e2d3
HiagoAC <hiago.uft@gmail.com>
2024-01-25 18:58:55 +0900

    created abstract model for commom attributes and methods of ingredient management models.

--- backend/app/ingredient/models.py
+++ backend/app/ingredient/models.py
@@ -5,6 +5,7 @@ Ingredient app models.
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models.constraints import UniqueConstraint
 
 from ingredient.measurement_units import DISPLAY_UNITS, MeasurementUnits
 
@@ -25,43 +26,79 @@ class Ingredient(models.Model):
         return self.name
 
 
-class IngredientInPantry(models.Model):
-    """Ingrendient a user has."""
+class IngredientManagementBase(models.Model):
+    """Base model for ingredients in different contexts."""
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    quantity = models.DecimalField(
-        max_digits=6, decimal_places=2, null=True, default=None)
+    quantity = models.DecimalField(max_digits=6, default=0, decimal_places=2)
     measurement_unit = models.CharField(
         max_length=3,
         choices=MeasurementUnits.choices,
         default=MeasurementUnits.UNIT
     )
-    expiration = models.DateField(null=True, default=None)
+    display_unit = models.CharField(max_length=20)
+
+    _ingredient_management_models = []
 
     class Meta:
-        unique_together = ['user', 'ingredient']
+        abstract = True
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        IngredientManagementBase._ingredient_management_models.append(cls)
+
+    @classmethod
+    def create_with_display_unit(cls, recipe, ingredient, display_unit,
+                                 quantity):
+        """
+        Create an instance with quantity in display unit.
+        measurement_unit used internally and converted quantity are set
+        accordingly.
+        """
+        if display_unit not in DISPLAY_UNITS:
+            raise ValidationError('Invalid display_unit.')
+
+        measurement_unit = DISPLAY_UNITS[display_unit].get_standard_unit()
+        quantity = DISPLAY_UNITS[display_unit].convert_quantity(quantity)
+
+        return cls.objects.create(
+            recipe=recipe,
+            ingredient=ingredient,
+            quantity=quantity,
+            measurement_unit=measurement_unit,
+            display_unit=display_unit
+        )
+
+    @classmethod
+    def get_ingredient_management_models(cls):
+        """Return a list of ingredient management models."""
+        return cls._ingredient_management_models
 
     def delete(self):
         """
-        Delete IngredientInPantry instance and Ingredient if it is not
-        associated with another IngredientInPantry or RecipeIngredient
-        instance.
+        Delete self and associated Ingredient instace if it is not associated
+        with any other instance.
         """
         ingredient = self.ingredient
         super().delete()
-        ing_pantry_exists = IngredientInPantry.objects.filter(
-            ingredient=ingredient).exists()
-        recipe_ing_exists = RecipeIngredient.objects.filter(
-            ingredient=ingredient).exists()
-        if not ing_pantry_exists and not recipe_ing_exists:
+        subclasses = self.__class__.get_ingredient_management_models()
+        used = False
+        for subclass in subclasses:
+            if subclass.objects.filter(ingredient=ingredient).exists():
+                used = True
+                break
+        if not used:
             ingredient.delete()
+
+    def get_display_quantity(self):
+        """Get quantity in display unit."""
+        return DISPLAY_UNITS[self.display_unit].convert_to_display_unit(
+            self.quantity)
 
     def subtract_quantity(self, sub_quantity, sub_unit):
         """
         Subtracts quantity of ingredient.
         - If subtracted quantity is greater or equal to the ingredient's
-        quantity the ingredient is deleted from pantry.
+        quantity the instance is deleted.
         - ValueError is raised if this method is called with a measurement
         unit different from the one of the object or if the quantity of the
         object has not been set.
@@ -83,64 +120,37 @@ class IngredientInPantry(models.Model):
 
         return self.quantity
 
+
+class IngredientInPantry(IngredientManagementBase):
+    """Ingrendient a user has."""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    expiration = models.DateField(null=True, default=None)
+    quantity = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, default=None)
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=['user', 'ingredient'],
+                name='unique_pantry_ingredient'
+                )
+            ]
+
     def __str__(self):
         return f'{self.ingredient.name} - {self.user.email}'
 
 
-class RecipeIngredient(models.Model):
+class RecipeIngredient(IngredientManagementBase):
     """Ingredient in a recipe."""
     recipe = models.ForeignKey('recipe.Recipe', on_delete=models.CASCADE)
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    quantity = models.DecimalField(max_digits=6, default=0, decimal_places=2)
-    measurement_unit = models.CharField(
-        max_length=3,
-        choices=MeasurementUnits.choices,
-        default=MeasurementUnits.UNIT
-    )
-    display_unit = models.CharField(max_length=20)
 
-    @classmethod
-    def create_with_display_unit(cls, recipe, ingredient, display_unit,
-                                 quantity):
-        """
-        Create an instance of RecipeIngredient with quantity in display unit.
-        Use this method instead of RecipeIngredient.objects.create().
-        measurement_unit used internally and converted quantity are set
-        accordingly.
-        """
-        if display_unit not in DISPLAY_UNITS:
-            raise ValidationError('Invalid display_unit.')
-
-        measurement_unit = DISPLAY_UNITS[display_unit].get_standard_unit()
-        quantity = DISPLAY_UNITS[display_unit].convert_quantity(quantity)
-
-        return cls.objects.create(
-            recipe=recipe,
-            ingredient=ingredient,
-            quantity=quantity,
-            measurement_unit=measurement_unit,
-            display_unit=display_unit
-        )
-
-    def get_display_quantity(self):
-        """Get quantity in display unit."""
-        return DISPLAY_UNITS[self.display_unit].convert_to_display_unit(
-            self.quantity)
-
-    def delete(self):
-        """
-        Delete RecipeIngredient instance and Ingredient if it is not
-        associated with another IngredientInPantry or RecipeIngredient
-        instance.
-        """
-        ingredient = self.ingredient
-        super().delete()
-        ing_pantry_exists = IngredientInPantry.objects.filter(
-            ingredient=ingredient).exists()
-        recipe_ing_exists = RecipeIngredient.objects.filter(
-            ingredient=ingredient).exists()
-        if not ing_pantry_exists and not recipe_ing_exists:
-            ingredient.delete()
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=['recipe', 'ingredient'],
+                name='unique_recipe_ingredient'
+                )
+            ]
 
     def __str__(self):
         return f'{str(self.ingredient)} in {str(self.recipe)}'
