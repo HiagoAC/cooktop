15265ba41c3ff5e584dd6ceebc0c27468425fbf1
HiagoAC <hiago.uft@gmail.com>
2024-02-02 21:53:36 +0900

    fixed media_url.

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/static/'
 MEDIA_URL = '/static/media/'
 
 MEDIA_ROOT = '/vol/web/media/'
