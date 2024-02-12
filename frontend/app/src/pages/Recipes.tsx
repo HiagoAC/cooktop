17391c59920d7ccaad7860b1d3324d174543e921
HiagoAC <hiago.uft@gmail.com>
2024-02-12 18:22:17 +0900

    linked buttons in Recipes to pages.

--- frontend/app/src/pages/Recipes.tsx
+++ frontend/app/src/pages/Recipes.tsx
@@ -9,9 +9,9 @@ export function Recipes() {
             <div className="page-title mt-3">Recipes</div>
             <Row className="justify-content-md-center g-6">
                 <Col className="button-col mt-2 mb-5">
-                    {['My Recipes', 'Add Recipe', 'Search'].map(text => (
-                        <Button className="mx-4 custom-button">{text}</Button>
-                    ))}
+                    <Button href="/recipes" className="mx-4 custom-button">My Recipes</Button>
+                    <Button href="/recipes/add" className="mx-4 custom-button">Add Recipe</Button>
+                    <Button href="/recipes/search" className="mx-4 custom-button">Search</Button>
                 </Col>
             </Row>
             <Row lg={3} md={2} xs={1} className="g-4">
