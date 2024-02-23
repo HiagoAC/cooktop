ba25cad8042abfe42a5009c2108b5d6a00fb97e2
HiagoAC <hiago.uft@gmail.com>
2024-02-23 18:34:19 +0900

    changed font size for texts in PantryIngredientCard.

--- frontend/app/src/components/PantryIngredientCard.tsx
+++ frontend/app/src/components/PantryIngredientCard.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { Button, Card, Col, Row } from 'react-bootstrap';
+import { PantryIngredientModal } from './PantryIngredientModal';
 import { PantryIngredient } from '../data/pantry';
 import editIcon from '../assets/edit_icon.svg';
 import trashBinIcon from '../assets/trash_bin_icon.svg';
@@ -10,14 +12,19 @@ interface Props {
 }
 
 export function PantryIngredientCard({ingredient}: Props) {
+    const [modalShow, setModalShow] = useState(false);
+
+    const handleModalClose = () => setModalShow(false);
+    const handleModalShow = () => setModalShow(true);
+
     return (
         <Card body className={`mb-2 ${styles.card}`}>
             <Row>
                 <Col>
-                    <div>
+                    <div className="fs-5">
                         {ingredient.name}: {ingredient.quantity} {ingredient.unit}
                     </div>
-                    <div>
+                    <div className="fs-6">
                         {
                             ingredient.expiration?
                             `expires in: ${ingredient.expiration}`
@@ -26,13 +33,20 @@ export function PantryIngredientCard({ingredient}: Props) {
                     </div>
                 </Col>
                 <Col className="d-flex justify-content-end">
-                    <Button className={`${styles.icon_button}`}>
+                    <Button className={`${styles.icon_button}`} onClick={handleModalShow}>
                         <img
                             src={editIcon}
                             alt="edit"
                             className={`${styles.icon}`}
                         />
                     </Button>
+                    <PantryIngredientModal
+                            show={modalShow}
+                            title="Edit pantry entry"
+                            buttonText="Save"
+                            handleClose={handleModalClose}
+                            handleClick={handleModalClose}
+                        />
                     <Button className={`${styles.icon_button}`}>
                         <img
                             src={trashBinIcon}
