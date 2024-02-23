168f86c4f461e218d3f48c6bfae41fe5c4626de9
HiagoAC <hiago.uft@gmail.com>
2024-02-23 11:02:08 +0900

    created basic layout of Pantry page.

--- frontend/app/src/pages/Pantry.tsx
+++ frontend/app/src/pages/Pantry.tsx
@@ -1,3 +1,20 @@
+import { Button } from 'react-bootstrap';
+import { PantryIngredient, pantryIngredients} from '../data/pantry';
+import '../styles/global.css';
+
+
 export function Pantry() {
-    return <h1>Pantry</h1>
+    return (
+        <>
+            <div className="page-title mt-3">Pantry</div>
+            <div className="d-flex justify-content-center mt-2 mb-5">
+                <Button className="custom-button">Add new item</Button>
+            </div>
+            <div>
+                {pantryIngredients.map((ingredient: PantryIngredient) => (
+                    <div className="mb-2" key={ingredient.id}>{ingredient.name}</div>
+                ))}
+            </div>
+        </>
+    )
 }
