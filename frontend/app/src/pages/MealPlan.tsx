68a52cace4a283f1a7b2f9074b87a45391b56c4e
HiagoAC <hiago.uft@gmail.com>
2024-02-25 11:20:31 +0900

    created MealPlan page and added basic elements.

--- frontend/app/src/pages/MealPlan.tsx
+++ frontend/app/src/pages/MealPlan.tsx
@@ -1,3 +1,22 @@
+import { Button } from 'react-bootstrap';
+import { mealPlan } from '../data/meal_plan';
+
+
 export function MealPlan() {
-    return <h1>MealPlan</h1>
+    const creationDate = mealPlan.creation_date;
+
+    return (
+        <>
+            <div className="page_title mt-3">
+                MealPlan {`for ${creationDate}`}
+            </div>
+            <div className="d-flex justify-content-center mt-2 mb-5 gap-4">
+                <Button
+                    className="custom_button"
+                >
+                    Make New Plan
+                </Button>
+            </div>
+        </>
+    )
 }
