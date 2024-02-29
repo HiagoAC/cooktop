6054ab14048d05904ba161dfee1a12b0b251bdc5
HiagoAC <hiago.uft@gmail.com>
2024-02-29 09:20:32 +0900

    created basic structure for scrapeRecipe function.

---
+++ frontend/app/src/utils/scrapeRecipe.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { Recipe } from '../data/recipe_detail';
+
+type Partial<T> = {
+    [K in keyof T]?: T[K];
+  };
+
+type RecipePartial = Partial<Recipe>;
+
+/**
+ * Scrape Recipe data from a URL.
+ * @param {string} url
+ * @returns {RecipePartial} A Recipe object with the data.
+ */
+export async function scrapeRecipe(url: string): Promise<RecipePartial> {
+    let recipe: RecipePartial = {};
+        const response = await axios.get(url);
+        const html = response.data;
+        console.log(html)
+        // #TODO: Call endpoint for scraping Recipe.
+    return recipe;
+}
