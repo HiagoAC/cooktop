399c7a78cfa1938cb8e45166d23506c77f92e177
HiagoAC <hiago.uft@gmail.com>
2024-03-01 16:48:54 +0900

    added button and alert modal for updating pantry and shopping list from meal plan.

--- frontend/app/src/pages/MealPlan.tsx
+++ frontend/app/src/pages/MealPlan.tsx
@@ -1,10 +1,12 @@
-import { Button, Container } from 'react-bootstrap';
+import { useState } from 'react';
+import { Button, Container, Modal } from 'react-bootstrap';
 import { MealCard } from '../components/MealCard';
 import { mealPlan } from '../data/meal_plan';
 import { formatDate } from '../utils/formatDate';
 
 
 export function MealPlan() {
+    const [alertShow, setAlertShow] = useState<boolean>(false);
     const creationDateObject = new Date(mealPlan.creation_date);
     const creationDate = formatDate(creationDateObject);
 
@@ -21,8 +23,38 @@ export function MealPlan() {
                 <Button
                     className="custom_button"
                 >
-                    Make New Plan
+                    {'Make New Plan'}
                 </Button>
+                <Button
+                    className="custom_button"
+                    onClick={() => setAlertShow(true)}
+                >
+                    {'Update Pantry & Shopping List'}
+                </Button>
+                <Modal show={alertShow} bg="warning">
+                    <Modal.Header>
+                        <Modal.Title>{'Alert!'}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <h5>{'Have you finished editing this meal plan?'}</h5>
+                        <p>{'You will have to update pantry and shopping list manually\
+                            if you make changes to this meal plan after clicking Yes.'}</p>
+                    </Modal.Body>
+                    <Modal.Footer className="d-flex justify-content-end">
+                        <Button
+                            onClick={() => setAlertShow(false)}
+                            variant="success"
+                        >
+                            {'Yes'}
+                        </Button>
+                        <Button
+                            onClick={() => setAlertShow(false)}
+                            variant="danger"
+                        >
+                            {'No'}
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
             </div>
             <div>
                 {Object.keys(mealPlan.meals).map((day: string) => (
