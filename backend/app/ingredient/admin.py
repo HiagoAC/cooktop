40f50000cdd4458c743481fb93a849995836d72a
HiagoAC <hiago.uft@gmail.com>
2024-04-15 13:32:19 +0900

    added RecipeIngredient and ShoppingListItem models to admin page.

--- backend/app/ingredient/admin.py
+++ backend/app/ingredient/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 
-from ingredient.models import Ingredient, IngredientInPantry
+from ingredient.models import (
+    Ingredient,
+    IngredientInPantry,
+    RecipeIngredient,
+    ShoppingListItem
+)
 
 
 admin.site.register(Ingredient)
 admin.site.register(IngredientInPantry)
+admin.site.register(RecipeIngredient)
+admin.site.register(ShoppingListItem)
