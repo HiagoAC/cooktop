4f9edc52b35614c57973e29d2701be0087024d79
HiagoAC <hiago.uft@gmail.com>
2024-07-06 16:45:47 +0900

    Created funciton to make a get request to meal-plans/current endpoint

--- frontend/app/src/api/mealPlansApi.ts
+++ frontend/app/src/api/mealPlansApi.ts
@@ -3,10 +3,17 @@ import { createMealPlanSchema } from './apiSchemas/mealPlansSchemas';
 import { BASE_URL } from './apiConfig';
 
 const MEAL_PLANS_URL: string = BASE_URL + 'meal-plans/';
+const CURRENT_PLAN_URL: string = MEAL_PLANS_URL + 'current/';
 
 
 export async function createMealPlan(data: createMealPlanSchema): Promise<any> {
     const response = await axios.post(MEAL_PLANS_URL, data);
     console.log(response);
     return response;        
+}
+
+export async function getMealPlan(): Promise<any> {
+    const response = await axios.get(CURRENT_PLAN_URL);
+    console.log(response);
+    return response;
 }
