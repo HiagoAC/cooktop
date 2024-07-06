0129d982573695aa66990e5c25e3dcc38495f1c0
HiagoAC <hiago.uft@gmail.com>
2024-07-06 19:01:45 +0900

    Added interfaces for Meal and MealPlan

--- frontend/app/src/types/interfaces.ts
+++ frontend/app/src/types/interfaces.ts
@@ -20,3 +20,16 @@ export interface Recipe {
     notes: string;
     ingredients: Ingredient[];
 }
+
+export interface Meal {
+    main_dish: Recipe;
+    side_dish: Recipe;
+    salad: Recipe; 
+}
+
+export interface MealPlan {
+    id: number;
+    servings_per_meal: number;
+    creation_date: string;
+    meals: Record<number, Meal>;
+}
