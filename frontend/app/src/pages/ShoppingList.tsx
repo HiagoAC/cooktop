7d6f2c1c32ee19b848601ce40bbf9cc7d600f733
HiagoAC <hiago.uft@gmail.com>
2024-07-09 09:38:16 +0900

    Added functionality to load shopping list from db and add new item to list from ShoppingList page

--- frontend/app/src/pages/ShoppingList.tsx
+++ frontend/app/src/pages/ShoppingList.tsx
@@ -1,16 +1,41 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button, Container } from 'react-bootstrap';
 import { ItemCard } from '../components/ItemCard';
 import { ShoppingItemModal } from '../components/ShoppingItemModal';
-import { ShoppingItem, shoppingList} from '../data/shopping_list';
 import styles from '../styles/ShoppingList.module.css';
+import { addItemToList, getShoppingList } from '../api/shoppingListApi';
+import { ShoppingListItem } from '../types/interfaces';
 
 
 export function ShoppingList() {
     const [modalShow, setModalShow] = useState(false);
+    const [shoppingList, setShoppingList] = useState<ShoppingListItem[]>([]);
 
     const handleModalClose = () => setModalShow(false);
     const handleModalShow = () => setModalShow(true);
+
+    const loadShoppingList = () => {
+        getShoppingList().then(res => {
+            console.log(res)
+            if (res.status == 200) {
+                setShoppingList(res.data);
+            }
+        });
+    };
+
+    const addItem = (shoppingItem: ShoppingListItem) => {
+        if (!shoppingItem) {
+            return;
+        }
+        addItemToList(shoppingItem).then(res => {
+            console.log(res);
+            handleModalClose();
+        });
+    };
+
+    useEffect (() => {
+        loadShoppingList();
+    }, []);
 
     return (
         <Container className="pb-4">
@@ -27,13 +52,16 @@ export function ShoppingList() {
                 </Button>
                 <ShoppingItemModal
                     show={modalShow}
+                    title={"Add a new item"}
+                    buttonText={"Save"}
+                    handleClick={addItem}
                     handleClose={handleModalClose}
                 />
             </div>
             <div className="d-flex justify-content-center">
                 <div className={`${styles.ingredient_list_container}`}>
-                    {shoppingList.map((item: ShoppingItem) => (
-                        <ItemCard key={item.id} item={item} />
+                    {shoppingList.map((item: ShoppingListItem) => (
+                        <ItemCard key={item.name} item={item} cardType={"SHOPPING"}/>
                     ))}
                 </div>
             </div>
